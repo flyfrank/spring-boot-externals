2b82bdfa9e5ddd032e907062eef2932ef60ecf25
huyaoke <huyaoke@wxchina.com>
2021-01-04 14:40:56 +0800

    first commit

---
+++ src/test/java/com/mpoom/springbootexternals/SpringBootExternalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpoom.springbootexternals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExternalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
